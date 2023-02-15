e40b33e78574751d3b1c1becb59581eef9790cc9
jacker1342 <jackerchaiwan@gmail.com>
2023-02-15 14:21:46 -0800

    add file negative_employee -close #4

--- backend/entity/employee.go
+++ backend/entity/employee.go
@@ -8,5 +8,5 @@ type Employee struct {
 	gorm.Model
 	Name       string `valid:"required~Name cannot be blank"` // ต้องไม่เป็นค่าว่าง
 	Email      string
-	EmployeeID string // รหัสพนักงานขึ7นต้นด้วย J หรือ M หรือ S แล้วตามด้วยตัวเลขจํานวน 8 ตัว
+	EmployeeID string 	`valid:"matches(^[JMS]{1}\\d{8}$)~EmployeeID is invalid"`	// รหัสพนักงานขึ7นต้นด้วย J หรือ M หรือ S แล้วตามด้วยตัวเลขจํานวน 8 ตัว
 }
